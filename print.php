59ab0069893718f95987b3d2ec1d6bba68415246
MatteoCarbone17 <matteocarbone17@live.com>
2023-01-24 16:51:19 +0100

    push

--- print.php
+++ print.php
@@ -1,15 +1,18 @@
-
 <p>
-<?php
-echo $_GET['testo']; 
-?>
-lunghezza testo :
-<?php
-echo strlen($_GET['testo']);
-?>
+    <?php
+    echo $_GET['testo'];
+    ?>
+    lunghezza testo :
+    <?php
+    echo strlen($_GET['testo']);
+    ?>
 </p>
-
 <?php
-echo  str_ireplace( 'js','***',$_GET['hiddenText'] );
-
+echo  str_ireplace('js', '***', $_GET['hiddenText']);
+?>
+<span>
+    lunghezza testo con hidden word :
+</span>
+<?php
+echo strlen($_GET['hiddenText']);
 ?>
